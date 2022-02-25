cd194a9bbed9bedca7a969916d209364327d44ea
moein9gh <moein9gh@gmail.com>
2022-02-25 12:26:25 +0330

    intilizing ...

---
+++ cmd/bmi.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// get inputs and calculate BMI
+func main() {
+
+	fmt.Println("BMI calculator")
+	fmt.Println("--------------------------")
+
+	//getting the inputs
+	fmt.Print("Please enter your weight (kg) :")
+	fmt.Print("Please enter your height (m) :")
+}
